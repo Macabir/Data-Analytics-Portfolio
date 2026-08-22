847fa9be0ce2a49c8a123b2fde3e9ca4e0105b16
Mason Cabirac <54755795+Macabir@users.noreply.github.com>
2026-08-22 10:36:57 -0500

    Delete Data Wrangling Examples/queries directory

--- Data Wrangling Examples/queries/05_try_cast.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW [dbo].[Final_Cafe_Sales_Check] AS
-SELECT 
-    [Transaction_ID],
-    [Cleaned_Item],
-    TRY_CAST([Cleaned_Quantity] AS FLOAT) AS [Cleaned_Quantity],
-    TRY_CAST([Cleaned_Price_Per_Unit] AS FLOAT) AS [Cleaned_Price_Per_Unit],
-    TRY_CAST([Cleaned_Total_Spent] AS FLOAT) AS [Cleaned_Total_Spent]
-FROM [dbo].[Cleaned_Items];
