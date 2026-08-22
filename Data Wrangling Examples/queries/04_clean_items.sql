477c396d18e2e59efbb3df7a7b090f9f6e29f5c0
Mason Cabirac <54755795+Macabir@users.noreply.github.com>
2026-08-22 10:45:56 -0500

    Add files via upload

---
+++ Data Wrangling Examples/queries/04_clean_items.sql
@@ -0,0 +1,21 @@
+CREATE VIEW [dbo].[Cleaned_Items] AS
+SELECT 
+    [Transaction_ID],
+
+	-- Map known prices, preserve valid items, and default the rest to 'UNKNOWN'
+		COALESCE(
+			CASE 
+				WHEN TRY_CAST([Cleaned_Price_Per_Unit] AS FLOAT) = 1 THEN 'Cookie'
+				WHEN TRY_CAST([Cleaned_Price_Per_Unit] AS FLOAT) = 1.5 THEN 'Tea'
+				WHEN TRY_CAST([Cleaned_Price_Per_Unit] AS FLOAT) = 2 THEN 'Coffee'
+				WHEN TRY_CAST([Cleaned_Price_Per_Unit] AS FLOAT) = 5 THEN 'Salad'
+				WHEN Item NOT IN ('ERROR', 'UNKNOWN') AND Item IS NOT NULL THEN Item
+				ELSE NULL
+			END, 
+			'UNKNOWN'
+		) AS [Cleaned_Item],
+  
+      [Cleaned_Quantity],
+      [Cleaned_Price_Per_Unit],
+      [Cleaned_Total_Spent]
+FROM [dbo].[Cleaned_Total_Spent];
